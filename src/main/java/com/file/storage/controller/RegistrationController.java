1bd57c31137dd9848eba37fd213dded1851daa09
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-24 15:40:21 +0700

    fix(controller): add missing @RequestBody annotation in `RegistrationController`

--- src/main/java/com/file/storage/controller/RegistrationController.java
+++ src/main/java/com/file/storage/controller/RegistrationController.java
@@ -7,8 +7,11 @@ import com.file.storage.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -18,9 +21,10 @@ public class RegistrationController {
     private final UserRepository userRepository;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> register(UserRequestDto userDto) {
+    public ResponseEntity<UserResponseDto> register(@RequestBody UserRequestDto userDto) {
         User user = new User(userDto.getLogin(), userDto.getPassword());
 
+        // TODO: 24-May-23 Extract to service + use ModelMapper
         User saved = userRepository.save(user);
 
         UserResponseDto userResponseDto = new UserResponseDto(
