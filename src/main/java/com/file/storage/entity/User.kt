6e615a8622c8bb1bcf39c6186899b7fd5db845a8
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-26 11:41:16 +0700

    fix(entity): add explicit name for table users, since user is a reserved word in a postgres

--- src/main/java/com/file/storage/entity/User.kt
+++ src/main/java/com/file/storage/entity/User.kt
@@ -2,10 +2,12 @@ package com.file.storage.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.annotations.UuidGenerator
 import java.util.*
 
 @Entity
+@Table(name = "users")
 data class User(
     @Id
     @UuidGenerator
