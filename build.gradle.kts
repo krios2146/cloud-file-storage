523d29fedc2d2f54d531c71f001ffff664766f86
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-29 16:32:11 +0700

    feat: add missing security dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-resource-server")
+    implementation("org.springframework.security:spring-security-oauth2-jose")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
     compileOnly("org.projectlombok:lombok")
