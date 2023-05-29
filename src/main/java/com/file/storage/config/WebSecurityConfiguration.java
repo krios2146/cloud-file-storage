7e9c66f707ea2c52fe9f06ab03f78ae5bf3ff313
Vladimir Kidyaev <krios2146@gmail.com>
2023-05-29 16:35:21 +0700

    refactor(config): delete unnecessary lines from WebSecurityConfiguration

--- src/main/java/com/file/storage/config/WebSecurityConfiguration.java
+++ src/main/java/com/file/storage/config/WebSecurityConfiguration.java
@@ -8,21 +8,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfiguration {
 
-    private static final String AUTH_ENDPOINT_URL = "/api/v1/auth";
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(POST, AUTH_ENDPOINT_URL + "/login", AUTH_ENDPOINT_URL + "/register")
-                        .permitAll()
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
                 )
                 .oauth2Login(withDefaults())
                 .csrf(csrf -> csrf.disable())
