3055cb79e5d163dc05ea9e8c49479355bf5f1912
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-07 11:04:59 +0700

    feat: add HelloController

---
+++ src/main/java/com/file/storage/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.file.storage.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public OidcUser hello(@AuthenticationPrincipal OidcUser user) {
+        return user;
+    }
+}
