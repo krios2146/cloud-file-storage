86172875c5e71f6c0ef609842104568f863edf5a
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-08 13:12:23 +0700

    feat(service): add UserService

---
+++ src/main/java/com/file/storage/service/UserService.java
@@ -0,0 +1,28 @@
+package com.file.storage.service;
+
+import com.file.storage.dto.UserRegistrationRequest;
+import com.file.storage.model.User;
+import com.file.storage.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+import static com.file.storage.model.Role.ROLE_USER;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public void register(UserRegistrationRequest userRegistrationRequest) {
+        User user = new User(
+                userRegistrationRequest.getUsername(),
+                userRegistrationRequest.getPassword(),
+                userRegistrationRequest.getEmail(),
+                Set.of(ROLE_USER)
+        );
+        userRepository.save(user);
+    }
+}
