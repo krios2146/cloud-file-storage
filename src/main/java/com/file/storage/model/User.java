5bea3a5b91bf2cb37fe2546832082b95d0a48525
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-08 13:11:08 +0700

    feat(model): add User model

---
+++ src/main/java/com/file/storage/model/User.java
@@ -0,0 +1,41 @@
+package com.file.storage.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    private Set<Role> roles;
+
+    public User(String username, String password, String email, Set<Role> roles) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+}
