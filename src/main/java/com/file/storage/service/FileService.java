92afbe9b7952b2486b52442e7298853d9a5d6e6d
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-12 15:23:10 +0700

    feat(service): add `uploadFiles()` method to the FileService for folder uploading

--- src/main/java/com/file/storage/service/FileService.java
+++ src/main/java/com/file/storage/service/FileService.java
@@ -4,11 +4,16 @@ import com.file.storage.MinioHelper;
 import com.file.storage.config.MinioBucketConfiguration;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
+import io.minio.SnowballObject;
+import io.minio.UploadSnowballObjectsArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,8 +30,39 @@ public class FileService {
                     .bucket(minioBucketConfiguration.getBucketName())
                     .object(minioHelper.getRootFolder() + file.getOriginalFilename())
                     .build());
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void uploadFiles(List<MultipartFile> files) {
+        try {
+            List<SnowballObject> objects = convertToObjects(files);
+
+            minioClient.uploadSnowballObjects(UploadSnowballObjectsArgs.builder()
+                    .bucket(minioBucketConfiguration.getBucketName())
+                    .objects(objects)
+                    .build());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<SnowballObject> convertToObjects(List<MultipartFile> files) throws IOException {
+        List<SnowballObject> objects = new ArrayList<>();
+
+        for (MultipartFile file : files) {
+            SnowballObject snowballObject = new SnowballObject(
+                    minioHelper.getRootFolder() + file.getOriginalFilename(),
+                    file.getInputStream(),
+                    file.getSize(),
+                    null
+            );
+            objects.add(snowballObject);
+        }
+
+        return objects;
     }
 }
