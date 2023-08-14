d55d7afe16bfa51900ae9539f5dda87f28a5c14a
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-14 12:11:02 +0700

    refactor: MinioHelper no more have access to SecurityContext

--- src/main/java/com/file/storage/MinioHelper.java
+++ src/main/java/com/file/storage/MinioHelper.java
@@ -1,16 +1,13 @@
 package com.file.storage;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class MinioHelper {
 
-    public String getRootFolder() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        return "user-" + auth.getName() + "-files/";
+    public String getRootFolderForUser(String username) {
+        return "user-" + username + "-files/";
     }
 }
