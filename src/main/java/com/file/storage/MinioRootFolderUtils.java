b5da55d03a356047ffc087b259cbf3c4c282b697
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-15 10:54:03 +0700

    feat(utils): add `removeUserRootFolderPrefix()` method to the MinioRootFolderUtils

--- src/main/java/com/file/storage/MinioRootFolderUtils.java
+++ src/main/java/com/file/storage/MinioRootFolderUtils.java
@@ -5,4 +5,10 @@ public class MinioRootFolderUtils {
     public static String getRootFolderForUser(String username) {
         return "user-" + username + "-files/";
     }
+
+    public static String removeUserRootFolderPrefix(String path, String username) {
+        String rootFolder = getRootFolderForUser(username);
+
+        return path.replaceAll(rootFolder, "");
+    }
 }
