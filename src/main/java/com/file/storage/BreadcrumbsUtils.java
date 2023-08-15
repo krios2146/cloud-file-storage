b7e92168c69a79f4a0675b7fa6de09bb03703a37
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-15 10:58:09 +0700

    refactor(utils): rename `getBreadcrumbsForPath()` to `getBreadcrumbLinksForPath()` & and change logic, so it fetches links to folders instead of names

--- src/main/java/com/file/storage/BreadcrumbsUtils.java
+++ src/main/java/com/file/storage/BreadcrumbsUtils.java
@@ -2,13 +2,28 @@ package com.file.storage;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class BreadcrumbsUtils {
 
-    public static List<String> getBreadcrumbsForPath(String path) {
+    public static List<String> getBreadcrumbLinksForPath(String path) {
+        if (path.isEmpty()) {
+            return List.of(path);
+        }
+
+        List<String> links = new ArrayList<>();
+
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == '/') {
+                links.add(path.substring(0, i));
+            }
+        }
+
+        return links;
+    }
         return Arrays.stream(path.split("/")).toList();
     }
 }
