fdc68d15826fef324a8d6927d24114bbb21e862f
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-16 13:59:11 +0700

    feat(dto): add FolderRenameRequest

---
+++ src/main/java/com/file/storage/dto/FolderRenameRequest.java
@@ -0,0 +1,21 @@
+package com.file.storage.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FolderRenameRequest {
+
+    private String currentName;
+
+    private String newName;
+
+    private String path;
+
+    private String owner;
+}
