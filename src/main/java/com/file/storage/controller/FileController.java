93c2c6942d02f7806e6516a338aadd203d30eb15
Vladimir Kidyaev <krios2146@gmail.com>
2023-08-17 09:22:41 +0700

    feat(controller): add get mapping to the FileController

--- src/main/java/com/file/storage/controller/FileController.java
+++ src/main/java/com/file/storage/controller/FileController.java
@@ -1,10 +1,14 @@
 package com.file.storage.controller;
 
 import com.file.storage.dto.FileDeleteRequest;
+import com.file.storage.dto.FileDownloadRequest;
 import com.file.storage.dto.FileRenameRequest;
 import com.file.storage.dto.FileUploadRequest;
 import com.file.storage.service.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +18,15 @@ import org.springframework.web.bind.annotation.*;
 public class FileController {
 
     private final FileService fileService;
+
+    @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<ByteArrayResource> downloadFile(@ModelAttribute("fileDownloadRequest") FileDownloadRequest fileDownloadRequest) {
+        ByteArrayResource file = fileService.downloadFile(fileDownloadRequest);
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=" + fileDownloadRequest.getName())
+                .body(file);
+    }
 
     @PostMapping
     public String uploadFile(@ModelAttribute("fileUploadRequest") FileUploadRequest fileUploadRequest) {
