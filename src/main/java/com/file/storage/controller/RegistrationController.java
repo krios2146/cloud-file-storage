786303a0f9df8d47d11bbad0e1c2c5d4e2df4131
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-07 16:44:13 +0700

    feat(controller): add exception handlers to the RegistrationController

--- src/main/java/com/file/storage/controller/RegistrationController.java
+++ src/main/java/com/file/storage/controller/RegistrationController.java
@@ -2,13 +2,15 @@ package com.file.storage.controller;
 
 import com.file.storage.dto.request.UserRegistrationRequest;
 import com.file.storage.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/registration")
@@ -24,9 +26,35 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String register(@ModelAttribute("userRegistrationRequest") UserRegistrationRequest userRegistrationRequest) {
-        // TODO: Request validation
+    public String register(@Valid @ModelAttribute("userRegistrationRequest") UserRegistrationRequest userRegistrationRequest) {
         userService.register(userRegistrationRequest);
         return "redirect:/login";
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ModelAndView handleRequestValidation(MethodArgumentNotValidException e) {
+        var modelAndView = new ModelAndView("registration");
+
+        String message = e.getBindingResult().getAllErrors().stream()
+                .findFirst()
+                .map(error -> error.getDefaultMessage())
+                .orElse(e.getBody().getDetail());
+
+        modelAndView.addObject("error", message);
+        modelAndView.setStatus(e.getStatusCode());
+        modelAndView.addObject("userRegistrationRequest", new UserRegistrationRequest());
+
+        return modelAndView;
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ModelAndView handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        var modelAndView = new ModelAndView("registration");
+
+        modelAndView.addObject("error", "User with such credentials already exists. Try to change username or/and email");
+        modelAndView.setStatus(HttpStatus.CONFLICT);
+        modelAndView.addObject("userRegistrationRequest", new UserRegistrationRequest());
+
+        return modelAndView;
     }
 }
