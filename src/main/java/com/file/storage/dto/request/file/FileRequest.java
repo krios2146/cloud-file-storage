f03881bf5635b384d1dd22669130f92e32c198a4
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-07 15:15:27 +0700

    feat(dto): add base abstract FileRequest

---
+++ src/main/java/com/file/storage/dto/request/file/FileRequest.java
@@ -0,0 +1,15 @@
+package com.file.storage.dto.request.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public abstract class FileRequest {
+
+    private String owner;
+}
