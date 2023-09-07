0418f15a6d2cbc4cff4c1c62d74cc713e43ef90e
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-07 16:43:45 +0700

    feat(dto): add validation annotations to the UserRegistrationRequest

--- src/main/java/com/file/storage/dto/request/UserRegistrationRequest.java
+++ src/main/java/com/file/storage/dto/request/UserRegistrationRequest.java
@@ -1,5 +1,8 @@
 package com.file.storage.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,14 @@ import lombok.Setter;
 @Setter
 public class UserRegistrationRequest {
 
+    @NotBlank
     private String username;
 
+    @NotBlank
+    @Size(min = 5, message = "Password must be longer than 5 symbols")
     private String password;
 
+    @NotBlank
+    @Email
     private String email;
 }
