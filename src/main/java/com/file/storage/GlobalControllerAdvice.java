a97920ee92154cdc010b5fae32fd2341c16a2419
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-08 12:15:06 +0700

    feat(exception): add GlobalControllerAdvice

---
+++ src/main/java/com/file/storage/GlobalControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.file.storage;
+
+import com.file.storage.dto.request.UserRegistrationRequest;
+import com.file.storage.exception.InvalidRegistrationRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(InvalidRegistrationRequestException.class)
+    public ModelAndView handleInvalidRegistrationRequest(InvalidRegistrationRequestException e) {
+        var model = new ModelAndView("registration");
+
+        model.addObject("error", e.getMessage());
+        model.addObject("userRegistrationRequest", new UserRegistrationRequest());
+        model.setStatus(HttpStatus.BAD_REQUEST);
+
+        return model;
+    }
+}
