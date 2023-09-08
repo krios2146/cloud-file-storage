7bab137ba73af61e4a552ae3079f1405a725da98
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-08 12:18:20 +0700

    refactor(controller): now propagates exception to the `GlobalControllerAdvice`
    Delete `@ExceptionHandler` methods & add `BindingResult` to the method signature.

--- src/main/java/com/file/storage/controller/RegistrationController.java
+++ src/main/java/com/file/storage/controller/RegistrationController.java
@@ -1,16 +1,19 @@
 package com.file.storage.controller;
 
 import com.file.storage.dto.request.UserRegistrationRequest;
+import com.file.storage.exception.InvalidRegistrationRequestException;
 import com.file.storage.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static com.file.storage.util.ValidationUtils.getErrorMessage;
 
 @Controller
 @RequestMapping("/registration")
@@ -22,39 +25,18 @@ public class RegistrationController {
     @GetMapping
     public String showRegistrationPage(Model model) {
         model.addAttribute("userRegistrationRequest", new UserRegistrationRequest());
+
         return "registration";
     }
 
     @PostMapping
-    public String register(@Valid @ModelAttribute("userRegistrationRequest") UserRegistrationRequest userRegistrationRequest) {
+    public String register(@Valid @ModelAttribute("userRegistrationRequest") UserRegistrationRequest userRegistrationRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new InvalidRegistrationRequestException(getErrorMessage(bindingResult));
+        }
+
         userService.register(userRegistrationRequest);
+
         return "redirect:/login";
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ModelAndView handleRequestValidation(MethodArgumentNotValidException e) {
-        var modelAndView = new ModelAndView("registration");
-
-        String message = e.getBindingResult().getAllErrors().stream()
-                .findFirst()
-                .map(error -> error.getDefaultMessage())
-                .orElse(e.getBody().getDetail());
-
-        modelAndView.addObject("error", message);
-        modelAndView.setStatus(e.getStatusCode());
-        modelAndView.addObject("userRegistrationRequest", new UserRegistrationRequest());
-
-        return modelAndView;
-    }
-
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public ModelAndView handleDataIntegrityViolation(DataIntegrityViolationException e) {
-        var modelAndView = new ModelAndView("registration");
-
-        modelAndView.addObject("error", "User with such credentials already exists. Try to change username or/and email");
-        modelAndView.setStatus(HttpStatus.CONFLICT);
-        modelAndView.addObject("userRegistrationRequest", new UserRegistrationRequest());
-
-        return modelAndView;
     }
 }
