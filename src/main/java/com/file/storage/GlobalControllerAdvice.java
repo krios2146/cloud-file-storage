6d6d1c002af89eeeb8b4b727c0635b17c20336f9
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-09 13:03:37 +0700

    feat(exception): add `FileRequestException` handler in the GlobalControllerAdvice

--- src/main/java/com/file/storage/GlobalControllerAdvice.java
+++ src/main/java/com/file/storage/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.file.storage;
 
 import com.file.storage.exception.InvalidUserRegistrationRequestException;
+import com.file.storage.exception.file.FileRequestException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,5 +15,12 @@ public class GlobalControllerAdvice {
         redirectAttributes.addFlashAttribute("error", e.getMessage());
 
         return new RedirectView("/registration", true);
+    }
+
+    @ExceptionHandler(FileRequestException.class)
+    public RedirectView handleInvalidFileRequests(FileRequestException e, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("error", e.getMessage());
+
+        return new RedirectView("/", true);
     }
 }
