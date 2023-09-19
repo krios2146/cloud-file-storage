f1c54477694b7957723cb401695a3cdf2dd40c26
Vladimir Kidyaev <krios2146@gmail.com>
2023-09-19 13:33:09 +0700

    feat(test): add more integration tests for each file operation in the FileServiceIntegrationTest

--- src/test/java/com/file/storage/service/FileServiceIntegrationTest.java
+++ src/test/java/com/file/storage/service/FileServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.file.storage.service;
 
 import com.file.storage.dto.MinioObjectDto;
+import com.file.storage.dto.file.FileDeleteRequest;
+import com.file.storage.dto.file.FileRenameRequest;
 import com.file.storage.dto.file.FileUploadRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.nio.charset.StandardCharsets;
@@ -19,6 +22,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("dev")
 class FileServiceIntegrationTest {
 
@@ -42,16 +46,76 @@ class FileServiceIntegrationTest {
                 "text/plain",
                 "Content".getBytes(StandardCharsets.UTF_8)
         );
-        FileUploadRequest fileUploadRequest = new FileUploadRequest();
-        fileUploadRequest.setFile(file);
-        fileUploadRequest.setOwner("user");
 
-        fileService.uploadFile(fileUploadRequest);
+        fileService.uploadFile(new FileUploadRequest("user", file));
 
         List<MinioObjectDto> files = fileService.getUserFiles("user", "");
         assertEquals(1, files.size());
         MinioObjectDto minioFile = files.get(0);
         assertEquals(file.getOriginalFilename(), minioFile.getName());
+    }
+
+    @Test
+    void deleteFile_deleteFileFromMinio() {
+        MockMultipartFile file = new MockMultipartFile(
+                "name",
+                "File to delete",
+                "text/plain",
+                "Content".getBytes(StandardCharsets.UTF_8)
+        );
+        fileService.uploadFile(new FileUploadRequest("user", file));
+
+        fileService.deleteFile(new FileDeleteRequest("user", "/File to delete"));
+
+        List<MinioObjectDto> files = fileService.getUserFiles("user", "");
+        long actual = files.stream()
+                .filter(f -> f.getName().equals("File to delete"))
+                .count();
+        assertEquals(0, actual);
+    }
+
+    @Test
+    void renameFile_renameFileInMinio() {
+        MockMultipartFile file = new MockMultipartFile(
+                "name",
+                "File to rename",
+                "text/plain",
+                "Content".getBytes(StandardCharsets.UTF_8)
+        );
+        fileService.uploadFile(new FileUploadRequest("user", file));
+
+        fileService.renameFile(new FileRenameRequest(
+                "user",
+                "File to rename",
+                "Renamed file",
+                "File to rename")
+        );
+
+        List<MinioObjectDto> files = fileService.getUserFiles("user", "");
+        long filesWithOldName = files.stream()
+                .filter(f -> f.getName().equals("File to rename"))
+                .count();
+        long filesWithNewName = files.stream()
+                .filter(f -> f.getName().equals("Renamed file"))
+                .count();
+        assertEquals(0, filesWithOldName);
+        assertEquals(1, filesWithNewName);
+    }
+
+    @Test
+    void searchUserFile_showOnlyUserFile() {
+        MockMultipartFile file = new MockMultipartFile(
+                "name",
+                "File to search",
+                "text/plain",
+                "Content".getBytes(StandardCharsets.UTF_8)
+        );
+        fileService.uploadFile(new FileUploadRequest("user", file));
+        fileService.uploadFile(new FileUploadRequest("admin", file));
+        fileService.uploadFile(new FileUploadRequest("root", file));
+
+        List<MinioObjectDto> files = fileService.getAllUserFiles("user", "");
+        assertEquals(1, files.size());
     }
 
     @DynamicPropertySource
